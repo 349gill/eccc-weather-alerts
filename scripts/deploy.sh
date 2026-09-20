1879dfc47898f3ec0cbf5308d6df547556417504
349gill <harsh.gill@yahoo.com>
2026-09-20 12:57:22 -0600

    Remove swap memory allocation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-swapon --show | grep -q /swap || {
-  sudo fallocate -l 5G /swap
-  sudo chmod 600 /swap
-  sudo mkswap /swap
-  sudo swapon /swap
-}
-
 . ~/.nvm/nvm.sh
 nvm use 20
 
